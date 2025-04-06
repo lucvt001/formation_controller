d2cf682bf41f14fcaa298c55815536d268f33c2e
Luc <luc001@e.ntu.edu.sg>
2025-04-06 22:35:26 +0000

    Sim and real launch file for convenience

---
+++ launch/agent_bringup_real.launch.py
@@ -0,0 +1,23 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    agent_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('formation_controller'), 'launch', 'agent_bringup.launch.py')
+        ), launch_arguments={
+            'ns': 'follower',
+            'use_gps': 'True',
+            'is_sam': 'False',
+            'is_real': 'True',
+            'run_rover': 'True',
+            'rosbag': 'True',
+        }.items()
+    )
+
+    return LaunchDescription([
+        agent_bringup,
+    ])
